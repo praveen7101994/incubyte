631cac72b18d534c4d9f6015cfe56529ef1cd44a
Praveen Patel <praveenpatelonline@gmail.com>
2024-12-03 12:31:41 +0530

    testing empty string

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,9 +1,8 @@
-// tests/sum.test.js
-const sum = (a = 0, b = 0) => {
-  if (!a && !b) return 0;
-  return a + b;
-};
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(3, 4)).toBe(7);
+test("returns 0 for an empty string", () => {
+  expect(add("")).toBe(0);
 });
+
+function add(numbers) {
+  if (!numbers) return 0;
+  return -1;
+}
